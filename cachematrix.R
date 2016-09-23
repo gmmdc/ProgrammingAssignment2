6b442c63501fbc8a961e3a67aeb900f39d65745a
Geoff <ed@growthcrafters.com>
2016-09-22 23:40:33 -0400

    updated R file and now committing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 ---------------------------
+## This pair of functions will cache the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix will create a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    
+    ## build the setter function
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    ## build the getter function
+    get <- function() x
+    
+    setInv <- function(inv) i <<- inv
+    getInv <- function() i
+    
+    ## Return the four functions
+    list(set = set, 
+         get = get, 
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve will compute the inverse of the special "matrix" returned by the function above
+## If the inverse has already been calculated then cacheSolve should retrieve the inverse from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## first initailize i by pulling it from the 'getInv' function passed in (from above)
+    ## if this function has not already been run, i will be 'NULL'
+    i <- x$getInv()
+
+    ## if the inverse of x is stored in cache --> Return it from cache
+    if(!is.null(i)){
+        message("getting cached data")
+        return(i)
+    }
+    
+    ## otherwise, calculate the inverse of x and return
+    data <- x$get()     ## first get the matrix called x and assign it var 'data'
+    i <- solve(data, ...)    ## run 'solve()' in 'data' to get the inverse matrix & assign to 'i'
+    x$setInv(i)         ## run setInv() to set the value of i to 'i' in the parent environment (cache)
+    i                   ## return i
 }
